ecca64c67c270d63d3cdb4e694d8e78f34f4075a
narayanakhadri <narayanakhadri@gmail.com>
2026-09-15 19:53:49 +0530

    cicd-1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello():
 @app.route('/echo/<name>')
 def echo(name):
     print(f"This was placed in the url: new-{name}")
-    val = {"new-name4": name}
+    val = {"new-cicd-1": name}
     return jsonify(val)
 
 
